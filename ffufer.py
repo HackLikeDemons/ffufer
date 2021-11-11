600233ce97c36aca790c710479e7b61538453f2a
Andreas Wienes <andreas.wienes@otto.de>
2021-11-11 13:43:52 +0100

    fixed naming the ouput file

--- ffufer.py
+++ ffufer.py
@@ -6,9 +6,7 @@ Author: Andreas Wienes - Hack Like Demons - https://twitter.com/AndreasWienes
 
 First draft: 2021-11-10
 '''
-
-# TODO: Fix output file name
-# TODO: Fix replacement in file name
+# TODO: Add POST method
 
 import argparse
 import os
@@ -19,14 +17,16 @@ parser.add_argument('input_file', help='a file with URLs line by line as input f
 parser.add_argument('--options')
 args = parser.parse_args()
 
-
-
 user_agent = '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"'
 word_list = '~/hacking/tools/wordlists/hld_parameter_names.txt' # add your default wordlist here
 input_file = args.input_file
 options = args.options
+if not options:
+    options = ""
 output_path = 'recon'
 output_path_exist = os.path.exists(output_path)
+
+print('\nFfufer - Batch parameter fuzzing based on ffuf\n')
 
 if output_path_exist:
    print("Found existing folder 'recon'. All ouput files will be saved there.")   
@@ -37,11 +37,13 @@ else:
 with open(input_file) as f:
     urls = [line.rstrip() for line in f]
 
-for target_url in urls:
+for target_url in urls:    
+    host_name = urllib.parse.urlparse(target_url).hostname.replace(".","_")
+    path_name = urllib.parse.urlparse(target_url).path.replace("/","_")
     target_url = target_url + "\?FUZZ\\=1"
-    host_name = urllib.parse.urlparse(target_url).netloc
-    output_file = 'ffuf_' + target_url.replace("/","_").replace(":","_").replace('.','_').replace('___','_').replace('__','_')
-    ffuf_command = f"ffuf -s -ac -c -t 50 -w {word_list} -H 'User-Agent: {user_agent}' -o {output_path}/{output_file}.html -of html -or {options} -u {target_url}"
+    output_file = f'ffuf_{host_name}-{path_name}'
+    
+    ffuf_command = f"ffuf -ac -c -t 50 -w {word_list} -H 'User-Agent: {user_agent}' -o {output_path}/{output_file}.html -of html {options} -u {target_url}"
     print(ffuf_command)
     os.system(ffuf_command)
 
